46af0f7004a8795bf11314a4e3b42c71394bbe6f
GKZ000 <yi.kritsada_st@tni.ac.th>
2017-02-03 08:50:49 +0700

    Create generate_list.py

---
+++ generate_list.py
@@ -0,0 +1,22 @@
+import random
+def generation_list():
+        alist = [x for x in range (random.randint(-10,10))]
+        return alist
+
+"""
+print a generation_list
+"""
+
+def printIt():
+    print(generation_list())
+    
+def main():
+    printIt()
+
+"""
+If this script file is called, it will run main() directly
+"""
+
+if __name__ == '__main__':
+    print("Test printIt():")
+    main()
